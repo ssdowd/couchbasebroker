5c7c43f537c56af4df0d80d86182647c4a02b5a0
Sean Dowd <dowds@avalonconsult.com>
2016-03-03 13:25:35 -0600

    update travis.yml (quiet test), update curl_broker tests

--- examples/curl_broker.sh
+++ examples/curl_broker.sh
@@ -1,44 +1,53 @@
 # Some example endpoint tests
 
-username=someuser
-password=somepass
+username=$1
+password=$2
 port=7326
 
-curl -X GET http://${username}:${password}@localhost:${port}/v2/catalog
+###---->> Get the catalog
+curl -u ${username}:${password} -X GET http://localhost:${port}/v2/catalog
 
-curl -X PUT http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-111 -d '{
-  "service_id":"service-guid-111",
-  "plan_id":"b4c881e6-92ff-11e5-8436-60f81dc0df0a",
-  "organization_guid": "org-guid",
-  "space_guid":"space-guid",
-  "parameters": {"ami_id":"ami-ecb68a84"}
-}' -H "Content-Type: application/json"
+###---->> Create 2 Couchbase instances (111 and 222, requires a real working bosh or docker behind the broker)
+# curl -u ${username}:${password} -X PUT http://localhost:${port}/v2/service_instances/instance_guid-111 -d '{
+#   "service_id":"service-guid-111",
+#   "plan_id":"b4c881e6-92ff-11e5-8436-60f81dc0df0a",
+#   "organization_guid": "org-guid",
+#   "space_guid":"space-guid",
+#   "parameters": {"ami_id":"ami-ecb68a84"}
+# }' -H "Content-Type: application/json"
+#
+# curl -u ${username}:${password} -X PUT http://localhost:${port}/v2/service_instances/instance_guid-222 -d '{
+#   "service_id":"service-guid-222",
+#   "plan_id":"cfe06e26-92ff-11e5-aaff-60f81dc0df0a",
+#   "organization_guid": "org-guid",
+#   "space_guid":"space-guid",
+#   "parameters": {}
+# }' -H "Content-Type: application/json"
+#
 
-curl -X PUT http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-222 -d '{
-  "service_id":"service-guid-222",
-  "plan_id":"cfe06e26-92ff-11e5-aaff-60f81dc0df0a",
-  "organization_guid": "org-guid",
-  "space_guid":"space-guid",
-  "parameters": {}
-}' -H "Content-Type: application/json"
+###---->> check on the status of those 2 service instances... may take a while...
+# curl -u ${username}:${password} -X GET http://localhost:${port}/v2/service_instances/instance_guid-111
+# curl -u ${username}:${password} -X GET http://localhost:${port}/v2/service_instances/instance_guid-222
+#
 
-curl -X GET http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-111
-curl -X GET http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-222
+###---->> Try to bind and app to each instance
+# curl -u ${username}:${password} -X PUT http://localhost:${port}/v2/service_instances/instance_guid-111/service_bindings/binding_guid-111 -d '{
+#   "plan_id":        "b4c881e6-92ff-11e5-8436-60f81dc0df0a",
+#   "service_id":     "service-guid-111",
+#   "app_guid":       "app-guid"
+# }' -H "Content-Type: application/json"
+#
+# curl -u ${username}:${password} -X PUT http://localhost:${port}/v2/service_instances/instance_guid-222/service_bindings/binding_guid-222 -d '{
+#   "plan_id":        "cfe06e26-92ff-11e5-aaff-60f81dc0df0a",
+#   "service_id":     "service-guid-222",
+#   "app_guid":       "app-guid"
+# }' -H "Content-Type: application/json"
+#
 
-curl -X PUT http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-111/service_bindings/binding_guid-111 -d '{
-  "plan_id":        "b4c881e6-92ff-11e5-8436-60f81dc0df0a",
-  "service_id":     "service-guid-111",
-  "app_guid":       "app-guid"
-}' -H "Content-Type: application/json"
+###---->> Delete the service binding and service instance 111...
+# curl -u ${username}:${password} -X DELETE http://localhost:${port}/v2/service_instances/instance_guid-111/service_bindings/binding_guid-111
+# curl -u ${username}:${password} -X DELETE http://localhost:${port}/v2/service_instances/instance_guid-111
 
-curl -X PUT http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-222/service_bindings/binding_guid-222 -d '{
-  "plan_id":        "cfe06e26-92ff-11e5-aaff-60f81dc0df0a",
-  "service_id":     "service-guid-222",
-  "app_guid":       "app-guid"
-}' -H "Content-Type: application/json"
-
-curl -X DELETE http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-111/service_bindings/binding_guid-111
-curl -X DELETE http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-111
-
-curl -X DELETE http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-222/service_bindings/binding_guid-222
-curl -X DELETE http://${username}:${password}@localhost:${port}/v2/service_instances/instance_guid-222
+###---->> Delete the service binding and service instance 222...
+# curl -u ${username}:${password} -X DELETE http://localhost:${port}/v2/service_instances/instance_guid-222/service_bindings/binding_guid-222
+# curl -u ${username}:${password} -X DELETE http://localhost:${port}/v2/service_instances/instance_guid-222
