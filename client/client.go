ba3eb76d6b8f31e6f5911f8cb9bbf7ffe9c05fab
Sean Dowd <dowds@avalonconsult.com>
2016-03-03 12:06:13 -0600

    more golint cleanup

--- client/client.go
+++ client/client.go
@@ -3,21 +3,22 @@ package client
 import (
 	model "github.com/ssdowd/couchbasebroker/model"
 )
-  
+
+// A Client implements the connection to some type of IaaS to provide services via a service broker.
 type Client interface {
 	CreateInstance(parameters interface{}) (string, error)
-	GetInstanceState(instanceId string) (string, error)
-	DeleteInstance(instanceId string) error
+	GetInstanceState(instanceID string) (string, error)
+	DeleteInstance(instanceID string) error
 
-  // new interface
-  GetCredentials(instanceId string) (*model.Credential,error)
-	RemoveCredentials(instanceId string, bindingId string) error
-  
-  // old SSH to a VM interface
-	InjectKeyPair(instanceId string) (string, string, string, error)
-	RevokeKeyPair(instanceId string, privateKey string) error
-  
-  SetCatalog(catalog *model.Catalog) error
-  GetCatalog() *model.Catalog
-  IsValidPlan(planName string) bool
+	// new interface
+	GetCredentials(instanceID string) (*model.Credential, error)
+	RemoveCredentials(instanceID string, bindingID string) error
+
+	// old SSH to a VM interface
+	InjectKeyPair(instanceID string) (string, string, string, error)
+	RevokeKeyPair(instanceID string, privateKey string) error
+
+	SetCatalog(catalog *model.Catalog) error
+	GetCatalog() *model.Catalog
+	IsValidPlan(planName string) bool
 }
